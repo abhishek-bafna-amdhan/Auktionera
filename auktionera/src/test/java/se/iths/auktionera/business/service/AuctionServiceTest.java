71dbd7f446efc4de8ca19217a4326dca7f1a1745
Petros Tsavdaridis <peteropon@gmail.com>
2020-01-09 14:45:12 +0100

    Added method that updates total sales and purchases after an item has been sold
    co-authored-by: bartekkozuszek@gmail.com

--- auktionera/src/test/java/se/iths/auktionera/business/service/AuctionServiceTest.java
+++ auktionera/src/test/java/se/iths/auktionera/business/service/AuctionServiceTest.java
@@ -12,6 +12,7 @@ import se.iths.auktionera.persistence.entity.AccountEntity;
 import se.iths.auktionera.persistence.entity.AuctionEntity;
 import se.iths.auktionera.persistence.repo.AccountRepo;
 import se.iths.auktionera.persistence.repo.AuctionRepo;
+import se.iths.auktionera.persistence.repo.UserStatsRepo;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -21,7 +22,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.when;
 
 @DataJpaTest
@@ -33,6 +33,9 @@ class AuctionServiceTest {
     @MockBean
     AccountRepo accountRepo;
 
+    @MockBean
+    UserStatsRepo userStatsRepo;
+
     private IAuctionService auctionService;
     private AuctionEntity auctionEntity1;
     private AuctionRequest auctionRequest;
@@ -42,7 +45,7 @@ class AuctionServiceTest {
     @BeforeEach
     void setUp() {
 
-        auctionService = new AuctionService(auctionRepo, accountRepo);
+        auctionService = new AuctionService(auctionRepo, accountRepo, userStatsRepo);
         accountEntity = AccountEntity.builder()
                 .id(10)
                 .anonymousBuyer(false)

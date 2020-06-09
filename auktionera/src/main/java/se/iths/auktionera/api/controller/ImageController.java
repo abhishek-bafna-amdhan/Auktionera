b11658a4015fc94370bfe75f9a967eb51a192652
Petros Tsavdaridis <peteropon@gmail.com>
2020-06-09 17:46:13 +0200

    Some refactoring in ImageController.

--- auktionera/src/main/java/se/iths/auktionera/api/controller/ImageController.java
+++ auktionera/src/main/java/se/iths/auktionera/api/controller/ImageController.java
@@ -1,5 +1,6 @@
 package se.iths.auktionera.api.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequestMapping("api/images/")
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class ImageController {
 
@@ -19,14 +21,16 @@ public class ImageController {
         this.imageService = imageService;
     }
 
-    @GetMapping("api/images/{id}")
+    @GetMapping("{id}")
     public List<Image> getImagesForAuction(@PathVariable Long id) {
         return imageService.getImagesForAuction(id);
     }
 
-    @PostMapping("api/images/upload/{id}")
-    public ResponseEntity.BodyBuilder storeUserImage(@PathVariable Long id, @RequestParam MultipartFile file) throws IOException {
+    @PostMapping(value = "upload/{id}",
+        consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<Image> storeUserImage(@PathVariable Long id, @RequestParam MultipartFile file) throws IOException {
         imageService.storeImage(id, file);
-        return ResponseEntity.ok();
+        return ResponseEntity.ok(new Image(file));
     }
 }
--- auktionera/src/main/java/se/iths/auktionera/business/model/Image.java
+++ auktionera/src/main/java/se/iths/auktionera/business/model/Image.java
@@ -1,7 +1,10 @@
 package se.iths.auktionera.business.model;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 import se.iths.auktionera.persistence.entity.ImageEntity;
+
+import java.io.IOException;
 
 @Getter
 @Setter
@@ -16,5 +19,10 @@ public class Image {
     public Image(ImageEntity i) {
         this.description = i.getDescription();
         this.data = i.getData();
+    }
+
+    public Image(MultipartFile file) throws IOException {
+        this.description = file.getName();
+        this.data = file.getBytes();
     }
 }

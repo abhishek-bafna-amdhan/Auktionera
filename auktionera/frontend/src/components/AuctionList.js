e76244a777c10e2eb897b4377a6aa83b8da17ea0
Petros Tsavdaridis <peteropon@gmail.com>
2020-06-27 16:40:25 +0200

    Added comp for auction form and another comp for text inputs.

--- auktionera/frontend/src/components/AuctionList.js
+++ auktionera/frontend/src/components/AuctionList.js
@@ -8,6 +8,7 @@ const AuctionList = (props) => {
           <tr>
             <th>Title</th>
             <th>Category</th>
+            <th>Seller</th>
             <th>Starting price</th>
             <th>Ends at</th>
             <th>Current bid</th>
@@ -15,11 +16,12 @@ const AuctionList = (props) => {
           </tr>
         </thead>
         <tbody>
-          {props.auctions.map(auction => {
+          {props.auctions.map((auction, index) => {
             return (
-              <tr key={auction.id}>
+              <tr key={index}>
                 <td>{auction.description}</td>
                 <td>{auction.category}</td>
+                <td>{auction.seller.userName}</td>
                 <td>{auction.startPrice}</td>
                 <td>{auction.endsAt}</td>
                 <td>{auction.currentBid}</td>

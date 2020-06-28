fb17bc408c563aab77d4606f82c3c524a17e1ebc
Petros Tsavdaridis <peteropon@gmail.com>
2020-06-28 17:27:08 +0200

    Added a 3rd party component for showing notifications after creating auction.

--- auktionera/frontend/src/App.js
+++ auktionera/frontend/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import { Switch , Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import login from "./login";
 import dashboard from "./Dashboard";
 import interceptors from "./Interceptors";
@@ -9,21 +9,24 @@ import NotFoundPage from "./NotFoundPage";
 import AuctionsPage from "./components/AuctionsPage";
 import AboutPage from "./components/AboutPage";
 import CreateAuctionPage from "./components/CreateAuctionPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-        <Switch>
-          <Route exact path="/" component={login} />
-          <Route path="/dashboard" component={dashboard} />
-          <Route path="/auctions" component={AuctionsPage}/>
-          <Route path="/newauction" component={CreateAuctionPage} />
-          <Route path="/about" component={AboutPage}/>
-          <Route component={NotFoundPage} />
-        </Switch>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ToastContainer autoClose={2500} hideProgressBar />
+            <Header />
+            <Switch>
+                <Route exact path="/" component={login} />
+                <Route path="/dashboard" component={dashboard} />
+                <Route path="/auctions" component={AuctionsPage} />
+                <Route path="/newauction" component={CreateAuctionPage} />
+                <Route path="/about" component={AboutPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
